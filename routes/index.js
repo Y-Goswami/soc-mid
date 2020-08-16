c5b999f87648cf9ea89af3b4eda7bb8b16899aa2
Yash Goswami <yashgoswami35@gmail.com>
2020-08-16 17:47:02 +0530

    Add a controller and a router for user

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ const homeController = require('../controllers/home_controller')
 
 console.log('router loaded');
 
+
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+// for any further routes, acess from here
+//router.use('/routerName'.require('./routerfile));
 
 module.exports = router;
