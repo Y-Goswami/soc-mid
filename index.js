59c13f0c8327c9a2b3ce1f40a49f77811ae17a6f
Yash Goswami <yashgoswami35@gmail.com>
2020-08-16 17:51:34 +0530

    set up view engine

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = 8000;
 //use express router
 app.use('/',require('./routes'));
 
+
+//set up view engine "ejs"
+app.set('veiw engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         // console.log('Error : ',err); 
