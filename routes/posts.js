8426f7e24d6b8647454d60089955d55bdbd07363
Yash Goswami <yashgoswami35@gmail.com>
2020-08-21 19:30:21 +0530

    saving user-post in database

---
+++ routes/posts.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+
+const postController = require('../controllers/posts_controller');
+
+router.post('/create',postController.create);
+
+module.exports =router;
