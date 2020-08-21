be60c7d8131fda9c177ee84727def1c3952897ec
Yash Goswami <yashgoswami35@gmail.com>
2020-08-22 00:29:25 +0530

    saving post in database

--- models/post.js
+++ models/post.js
@@ -14,10 +14,12 @@ const postSchema = new mongoose.Schema({
     //include the array of the ids of all comments in this post 
     // schema itself
 
-    comments:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'comment' 
-    }
+    comments:[
+            {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment' 
+        }
+    ]
 },{
     timestamps: true
 });
