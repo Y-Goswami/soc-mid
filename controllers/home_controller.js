756fe6b701b2dec76eef8a6111dd71a4d983a727
Yash Goswami <yashgoswami35@gmail.com>
2020-08-21 20:48:52 +0530

    display post with the  user name

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,27 @@
+const Post = require('../models/post');
 
 module.exports.home = function(req,res){
     // return res.end('<h1> Express is up for codial <!h1>');
-    return res.render('home',{
-        title: "Home"
+
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title: "Codeial Home",
+    //         posts: posts
+    //     });
+    // });
+
+// in place of the above code, the follwing code is written so that we can display
+// the name of the user with their post
+// populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: "Codeial Home",
+            posts: posts
+        });
     });
+
+
+   
 }
 
 //to add new actions
