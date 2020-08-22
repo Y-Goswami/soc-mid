0d269ef3c521674bb175785732619c646d7da7ae
Yash Goswami <yashgoswami35@gmail.com>
2020-08-22 14:00:26 +0530

    display comments and users

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -13,7 +13,15 @@ module.exports.home = function(req,res){
 // in place of the above code, the follwing code is written so that we can display
 // the name of the user with their post
 // populate the user of each post
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts){
         return res.render('home',{
             title: "Codeial Home",
             posts: posts
