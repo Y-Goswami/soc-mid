3a2a04317a65fe17e9cdaf4b81859800ef55e922
Yash Goswami <yashgoswami35@gmail.com>
2020-08-30 18:14:48 +0530

    Adding social authentication (google)

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport_local_strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
+
 const MongoStore = require('connect-mongo')(session);
 
 //use SASS
