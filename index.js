4a98cca4de16422acf3335e3b4c95ce1fff460f7
Yash Goswami <yashgoswami35@gmail.com>
2020-09-03 19:09:38 +0530

    added chatting feature

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ const sassMiddleware = require('node-sass-middleware');
 
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+// const { Server } = require('http');
+
+
+//next two lines for the chat feature, set up the chat server to be used woth socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000')
 
 app.use(sassMiddleware({
     src: './assets/scss',
